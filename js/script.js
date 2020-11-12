53673e8eebb5195babe8034b5fe65c9084461a12
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-11-12 15:21:56 +0100

    end project

--- js/script.js
+++ js/script.js
@@ -4,4 +4,13 @@ $(document).ready(function() {
     $(".primary-nav li").toggle();
     $(".primary-nav li:first-child").toggle();
   });
+
+  var div = $( ".carousel-after" );
+  $( ".carousel-after" ).resizable({
+    "maxWidth": $( ".carousel-after" ).offsetWidth,
+    "minWidth": 0,
+    "maxHeight": $( ".carousel-after" ).offsetHeight,
+    "minHeight": $( ".carousel-after" ).offsetHeight,
+    "handles": "e"
+  });
 });
